2a1dd9dbc6981265680f4f008f4c884ed49a1116
plantysnake <plantysnake@gmail.com>
2019-04-10 14:33:53 +0300

    Update f.py

--- f.py
+++ f.py
@@ -1,23 +1,29 @@
 def f():
-    import msvcrt
+    import msvcrt, os, pdb
     strr = b''''''
     inp = []
     fl = 1
     print(r"Enter your func below. Remember, that tabs are 4 ' ' and exit is '`'")
     while fl:
         input_char = msvcrt.getch()
-        if (not input_char == b"\x08") and (not input_char == b"`") and (not input_char == b"\r") and (not input_char == b"\t"):
+        if (not input_char == b"\x08") and (not input_char == b"`") and (not input_char == b"\r") and (not input_char == b"\t") and (not input_char == b"\xe0"):
             inp.append(input_char)
         elif (input_char == b"\x08"):
-            tmp = inp.pop()
+            try:
+                tmp = inp.pop()
+            except IndexError:
+                pass
         elif (input_char == b"\r"):
             inp.append(b"\n")
-            print(flush = True)
+            #print(flush = True)
         elif (input_char == b"\t"):
             for i in range(4):
                 inp.append(b" ")
-            print("    ", flush = True, end = "", sep = "")
-        print(input_char.decode(), end = "", flush = True)
+        os.system("cls")
+        for i in inp:
+            print(i.decode(), end = "", flush = True)
+            #print("    ", flush = True, end = "", sep = "")
+        #print(input_char.decode(), end = "", flush = True)
         if input_char.upper() == b'`':
             print ('OK')
             fl = 0
@@ -25,7 +31,10 @@ def f():
 
     strr = b"""""".join(inp)
     strr = strr.decode()
-    return strr
+    #return strr
     print(strr)
     exec (strr, globals(), locals())
+    pdb.set_trace()
     func()
+
+f()
